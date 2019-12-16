96e542089f20de6f51f974bac6bc7ecd3457797a
Mathias Vansevenant <mathias.vansevenant@student.hogent.be>
2019-12-16 12:49:45 +0100

    Add project files.

---
+++ BeerhallIEF/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerhallIEF.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring =
+                @"Server =.;Database=Beerhall;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
---
+++ BeerhallIEF/Program.cs
@@ -0,0 +1,18 @@
+﻿using BeerhallIEF.Data;
+using System;
+
+namespace BeerhallIEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ApplicationDbContext context = new ApplicationDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                Console.WriteLine("Database created");
+            }
+        }
+    }
+}
